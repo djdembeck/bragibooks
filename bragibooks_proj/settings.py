32e05e41ca4ae86d27d5949ccd2d0a4c1ca1f1c0
djdembeck <dembeck.david@protonmail.com>
2021-06-22 10:52:00 -0500

    Add a config dir for docker

--- bragibooks_proj/settings.py
+++ bragibooks_proj/settings.py
@@ -15,6 +15,11 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
+# config section for docker
+if os.path.isdir("/config"):
+	CONFIG_DIR = os.path("/config")
+else:
+	CONFIG_DIR = BASE_DIR
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
@@ -27,7 +32,6 @@ with open('secret_key.txt') as f:
 DEBUG = True
 
 ALLOWED_HOSTS = ['*']
-
 
 # Application definition
 
@@ -71,17 +75,15 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'bragibooks_proj.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
 DATABASES = {
 	'default': {
 		'ENGINE': 'django.db.backends.sqlite3',
-		'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+		'NAME': os.path.join(CONFIG_DIR, 'db.sqlite3'),
 	}
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators
@@ -101,7 +103,6 @@ AUTH_PASSWORD_VALIDATORS = [
 	},
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/2.2/topics/i18n/
 
@@ -114,7 +115,6 @@ USE_I18N = True
 USE_L10N = True
 
 USE_TZ = True
-
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
