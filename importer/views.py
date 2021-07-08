98e5da7f5622ed33de11ea29c58e77fc0b1214aa
djdembeck <dembeck.david@protonmail.com>
2021-07-07 21:39:24 -0500

    Add Audible chapter usage

--- importer/views.py
+++ importer/views.py
@@ -63,9 +63,10 @@ def make_models(asin, input_data):
     # Create BookData object from asin response
     aud = audible_helper.BookData(asin)
     metadata = aud.parser()
+    chapters = aud.get_chapters()
 
     # Process metadata and run components to merge files
-    m4b = m4b_helper.M4bMerge(input_data, metadata)
+    m4b = m4b_helper.M4bMerge(input_data, metadata, chapters)
     m4b.run_merge()
 
     # Book DB entry
