0969087b1f96e3dd4e81960e938329e1758dc9b2
djdembeck <dembeck.david@protonmail.com>
2022-01-09 20:05:32 -0600

    feat: :lipstick: add versions to footer
    also add migration for genre model deletion

--- bragibooks_proj/settings.py
+++ bragibooks_proj/settings.py
@@ -70,6 +70,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'importer.context_processors.add_version_to_context',
             ],
         },
     },
