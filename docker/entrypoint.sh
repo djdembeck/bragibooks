a1d9692630e03843e249840a2d060feb83662d83
David Dembeck <71412966+djdembeck@users.noreply.github.com>
2022-09-21 17:44:55 -0500

    chore(release): v0.3.6 (#110)
    * chore: Upgrade actions/checkout action to v3
    * chore: Upgrade docker/build-push-action digest to e27bcee
    * chore: Upgrade docker/login-action digest to 21f251a
    * chore: Upgrade docker/metadata-action digest to 59bc9dd
    * chore: Upgrade webiny/action-conventional-commits action to v1.0.5
    
    * build(docker): :building_construction: use gosu for startup perms
    
    * ci(docker): :construction_worker: try to use `workflow_dispatch` to rebuild when m4b-merge updates
    
    * fix: :bug: pass original path to `m4b-merge` so it knows what to move to the completed folder

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -4,13 +4,8 @@ USER_ID=${UID:-99}
 GROUP_ID=${GID:-100}
 
 echo "Starting with UID: $USER_ID, GID: $GROUP_ID"
-useradd -u "$USER_ID" -o -m user > /dev/null
-if [ $(getent group "$GROUP_ID") ]; then
-    usermod -g "$GROUP_ID" user > /dev/null
-else
-    groupmod -g "$GROUP_ID" user > /dev/null
-fi
-export HOME=/home/user
+# Fix permissions
 chown -R "$USER_ID":"$GROUP_ID" /config /input /output
 
-exec runuser -u user -- "$@"
+# Run the command as the specified user
+gosu "$USER_ID":"$GROUP_ID" "$@"
