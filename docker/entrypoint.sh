b3bd8a765c040ce14d57e0b483fdf689669b976b
djdembeck <dembeck.david@protonmail.com>
2023-04-18 14:12:03 -0500

    fix: :bug: entrypoint wasn't setting user permissions for processes

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -7,9 +7,6 @@ echo "Starting with UID: $USER_ID, GID: $GROUP_ID"
 # Fix permissions
 chown -R "$USER_ID":"$GROUP_ID" /config /input /output
 
-# Run the command as the specified user
-gosu "$USER_ID":"$GROUP_ID" "$@"
-
 until cd /home/app/web
 do
     echo "Waiting for server volume..."
@@ -17,22 +14,22 @@ do
 done
 
 
-until python manage.py migrate
+until gosu "$USER_ID":"$GROUP_ID" python manage.py migrate
 do
     echo "Waiting for db to be ready..."
     sleep 2
 done
 
-python manage.py collectstatic --noinput
+gosu "$USER_ID":"$GROUP_ID"python manage.py collectstatic --noinput
 
 # Start Celery Worker
-celery -A bragibooks_proj worker --loglevel=info --concurrency 1 -E &
+gosu "$USER_ID":"$GROUP_ID" celery -A bragibooks_proj worker --loglevel=info --concurrency 1 -E &
 
 # If you want to use the admin panel for debugging
 # python manage.py createsuperuser --noinput
 
 # Start gunicorn server
-gunicorn bragibooks_proj.wsgi \
+gosu "$USER_ID":"$GROUP_ID" gunicorn bragibooks_proj.wsgi \
     --bind 0.0.0.0:8000 \
     --timeout 1200 \
     --worker-tmp-dir /dev/shm \
